c12ed822cd41256c9edb8765c23890542d4ea25e
ryanstaudhammer <96213417+ryanstaudhammer@users.noreply.github.com>
2022-02-10 12:28:34 -0800

    Add files via upload

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -1,6 +1,6 @@
 -- retirement_titles table
 SELECT e.emp_no,
-    e.first_name,
+    	e.first_name,
 	e.last_name,
 	t.title,
 	t.from_date,
@@ -37,7 +37,7 @@ SELECT * FROM retiring_titles;
 -- mentorship_eligibility table 
 SELECT DISTINCT ON (e.emp_no)
 	e.emp_no,
-    e.first_name,
+    	e.first_name,
 	e.last_name,
 	e.birth_date,
 	de.from_date,
@@ -52,3 +52,22 @@ ON (de.emp_no = e.emp_no)
 WHERE (de.to_date = '9999-01-01') AND
 (birth_date BETWEEN '1965-01-01' AND '1965-12-31');
 SELECT * FROM mentorship_eligibility;
+
+-- mentorship_eligibility_extended table 
+SELECT DISTINCT ON (e.emp_no)
+	e.emp_no,
+    	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibility_extended
+FROM employees as e
+INNER JOIN titles as t 
+ON (e.emp_no = t.emp_no)
+LEFT JOIN dept_emp as de
+ON (de.emp_no = e.emp_no)
+WHERE (de.to_date = '9999-01-01') AND
+(birth_date BETWEEN '1965-01-01' AND '1975-12-31');
+SELECT * FROM mentorship_eligibility_extended;
